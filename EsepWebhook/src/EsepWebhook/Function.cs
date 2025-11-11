6dff9d241911a09e23ef308ab6f5489d7b789c99
Corey <flores.bmichelle@ufl.eduCoreygit config --global user.name Coreygit pushlsgit config --global user.email flores.bmichelle@ufl.eduCorey>
2025-11-10 22:33:56 -0500

    Function.cs update

---
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -0,0 +1,37 @@
+using System.Text;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EsepWebhook;
+
+public class Function
+{
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+    {
+        dynamic deserialize = JsonConvert.DeserializeObject(input.Body);
+
+        string payload = JsonConvert.SerializeObject(new { text = $"Issue Created: {deserialize.issue.html_url}" });
+
+        var client = new HttpClient();
+
+        var environmentVariable = Environment.GetEnvironmentVariable("SLACK_URL");
+        var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+        var response = client.PostAsync(
+            environmentVariable,
+            content
+        ).Result;
+
+        var proxyResponse = new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = response.Content.ReadAsStringAsync().Result,
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+
+        return proxyResponse;
+    }
+}
